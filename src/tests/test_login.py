e434b482021a98ba3382406ac26213c7c2b59119
Gerardo1909 <gerardotoboso1909@gmail.com>
2025-11-06 17:23:25 -0300

    refactor(src/tests/test_login.py): Se refactorizan pruebas unitarias para la página de inicio de sesión.

--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -4,6 +4,7 @@ Tests para el flujo de login de https://www.saucedemo.com/
 
 import pytest
 from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 
 
 def test_login_should_succeed_when_valid_credentials(selenium_driver):
@@ -11,12 +12,11 @@ def test_login_should_succeed_when_valid_credentials(selenium_driver):
     Prueba que verifica el inicio de sesión con credenciales válidas.
     """
     # Arrange
-    selenium_driver.get("https://www.saucedemo.com")
+    page = LoginPage(selenium_driver)
+    page.open()
 
     # Act
-    selenium_driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    selenium_driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    selenium_driver.find_element(By.ID, "login-button").click()
+    page.do_complete_login("standard_user", "secret_sauce")
 
     # Assert
     assert "/inventory.html" in selenium_driver.current_url
@@ -28,14 +28,11 @@ def test_login_should_fail_when_invalid_credentials(selenium_driver):
     Prueba que verifica el inicio de sesión con credenciales inválidas.
     """
     # Arrange
-    selenium_driver.get("https://www.saucedemo.com")
+    page = LoginPage(selenium_driver)
+    page.open()
 
     # Act
-    selenium_driver.find_element(By.ID, "user-name").send_keys("invalid_user")
-    selenium_driver.find_element(By.ID, "password").send_keys("wrong_password")
-    selenium_driver.find_element(By.ID, "login-button").click()
+    page.do_complete_login("invalid_user", "wrong_password")
 
     # Assert
-    assert selenium_driver.find_element(
-        By.CSS_SELECTOR, 'h3[data-test="error"]'
-    ).is_displayed()
+    assert page.error_is_displayed()
